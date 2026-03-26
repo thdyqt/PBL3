3677b00d004f062aae1930f01b2fa1003497f579
thdyqt <ptd.ntn2907@gmail.com>
2026-03-26 12:01:06 +0700

    Cap nhat class StaffData va table Staff

--- src/main/java/Data/StaffData.java
+++ src/main/java/Data/StaffData.java
@@ -1,4 +1,24 @@
 package Data;
+import Entity.Staff;
+import Util.DBConnection;
+import java.sql.*;
+import java.util.*;
 
 public class StaffData {
+    public List<Staff> getAllStaff(){
+        List<Staff> list = new ArrayList<>();
+        try{
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM staff");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                list.add(new Staff(rs.getString(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4), rs.getString(5),
+                        rs.getTimestamp(6).toLocalDateTime()));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
 }
--- src/main/java/Entity/Staff.java
+++ src/main/java/Entity/Staff.java
@@ -1,16 +1,19 @@
 package Entity;
+import java.time.*;
 
 public class Staff extends Person{
     private String user;
     private String password;
     private String role;
+    private LocalDateTime hire_date;
 
     public Staff() {}
-    public Staff(String phone, String name, String user, String password, String role) {
+    public Staff(String phone, String name, String user, String password, String role, LocalDateTime hire_date) {
         super(phone, name);
         this.user = user;
         this.password = password;
         this.role = role;
+        this.hire_date = hire_date;
     }
 
     public String getUser() {
@@ -35,5 +38,13 @@ public class Staff extends Person{
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public LocalDateTime getHire_date() {
+        return hire_date;
+    }
+
+    public void setHire_date(LocalDateTime hire_date) {
+        this.hire_date = hire_date;
     }
 }
