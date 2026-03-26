d76e6f2da8cecc84861ae79590ae024132a37444
hieutuoitre <hieu80180@gmail.com>
2026-03-26 22:32:51 +0700

    Cap nhat Customer.java

--- src/main/java/Entity/Customer.java
+++ src/main/java/Entity/Customer.java
@@ -1,4 +1,47 @@
 package Entity;
 
-public class Customer {
+public class Customer extends Person {
+    private String user;
+    private String password;
+    private int point;
+
+    public Customer() {}
+
+    public Customer(int id, String phone, String name, String user, String password) {
+        super(id, phone, name);
+        this.user = user;
+        this.password = password;
+        this.point = 0;
+    }
+
+    public Customer(String phone, String name, String user, String password ) {
+        super(phone, name);
+        this.user = user;
+        this.password = password;
+        this.point = 0;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
