15485138638aa664c76c9b122fb3408fe1849514
thdyqt <ptd.ntn2907@gmail.com>
2026-03-27 10:01:39 +0700

    Fix class Person, Staff, and Customer

--- src/main/java/Entity/Customer.java
+++ src/main/java/Entity/Customer.java
@@ -1,40 +1,18 @@
 package Entity;
 
 public class Customer extends Person {
-    private String user;
-    private String password;
     private int point;
 
     public Customer() {}
 
     public Customer(int id, String phone, String name, String user, String password, int point) {
-        super(id, phone, name);
-        this.user = user;
-        this.password = password;
+        super(id, phone, name, user, password);
         this.point = point;
     }
 
     public Customer(String phone, String name, String user, String password, int point ) {
-        super(phone, name);
-        this.user = user;
-        this.password = password;
+        super(phone, name, user, password);
         this.point = point;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public int getPoint() {
--- src/main/java/Entity/Staff.java
+++ src/main/java/Entity/Staff.java
@@ -11,35 +11,15 @@ public class Staff extends Person {
     public Staff() {}
 
     public Staff(int id, String phone, String name, String user, String password, String role, Date hire_date) {
-        super(id, phone, name);
-        this.user = user;
-        this.password = password;
+        super(id, phone, name, user, password);
         this.role = role;
         this.hire_date = hire_date;
     }
 
     public Staff(String phone, String name, String user, String password, String role, Date hire_date) {
-        super(phone, name);
-        this.user = user;
-        this.password = password;
+        super(phone, name, user, password);
         this.role = role;
         this.hire_date = hire_date;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getRole() {
