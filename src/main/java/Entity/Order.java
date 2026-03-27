c38e1f3ef43e80fba540aef737ecc6f259f814e0
sunny <huynhhieunghiadn@gmail.com>
2026-03-27 16:59:39 +0700

    added attributes to class Order

--- src/main/java/Entity/Order.java
+++ src/main/java/Entity/Order.java
@@ -1,4 +1,13 @@
 package Entity;
 
+import java.time.LocalDateTime;
+
 public class Order {
+    //attributes
+    private int id;
+    private LocalDateTime process_time;
+    private String customer_name;
+    private String staff_name;
+
+
 }
