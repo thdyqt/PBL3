ffa8da5096f2414bd31426da4d2bd63c89c3a802
sunny <huynhhieunghiadn@gmail.com>
2026-03-27 17:22:34 +0700

    created class Category and Product, added attributes to Order

--- src/main/java/Entity/Order.java
+++ src/main/java/Entity/Order.java
@@ -6,8 +6,21 @@ public class Order {
     //attributes
     private int id;
     private LocalDateTime process_time;
-    private String customer_name;
-    private String staff_name;
+    private Staff staff;
+    private Customer customer;
+    private OrderDetail detail;
+
+    //constructors
+    public Order(){};
+
+    public Order(int id, LocalDateTime process_time, Staff staff, Customer customer, OrderDetail detail){
+        this.id = id;
+        this.process_time = process_time;
+        this.staff = staff;
+        this.customer = customer;
+        this.detail = detail;
+    }
+
 
 
 }
--- src/main/java/Entity/OrderDetail.java
+++ src/main/java/Entity/OrderDetail.java
@@ -1,4 +1,8 @@
 package Entity;
 
+import java.util.List;
+
 public class OrderDetail {
+    //attributes
+    private List<Product> product_list;
 }
