289c2ac1a3b0ffc1736f2e9744e425c02df3781c
sunny <huynhhieunghiadn@gmail.com>
2026-03-30 21:11:23 +0700

    Added new entity: PromoCode

---
+++ src/main/java/Entity/PromoCode.java
@@ -0,0 +1,34 @@
+package Entity;
+
+import java.time.LocalDateTime;
+
+public class PromoCode {
+    //attributes
+    private String code;
+    private LocalDateTime expirationDate;
+    private double priceModifier;
+
+    //constructors
+    PromoCode(){}
+    PromoCode(String code, LocalDateTime expirationDate, double priceModifier) {
+        this.code = code;
+        this.expirationDate = expirationDate;
+        this.priceModifier = priceModifier;
+    }
+
+    //get-set
+    public String getCode() {return this.code;}
+    public void setCode(String code){this.code = code;}
+
+    public LocalDateTime getExpirationDate(){return this.expirationDate;}
+    public void setExpirationDate(LocalDateTime expirationDate) {this.expirationDate = expirationDate;}
+
+    public double getPriceModifier(){return this.priceModifier;}
+    public void setPriceModifier(double priceModifier){
+        if (priceModifier <= 0 || priceModifier > 1){
+            throw new IllegalArgumentException("Price modifier can only be larger 0 lower 1.");
+        }
+        this.priceModifier = priceModifier;
+    }
+
+}
